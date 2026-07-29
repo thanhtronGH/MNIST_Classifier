6598bdcb8108768bfea105917ef4ab772c85c739
Trong NguyenThanh <130883179+thanhtronGH@users.noreply.github.com>
2026-07-29 09:33:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
-import streamlit as st
-import RF_Model as model
-from PIL import Image
-import numpy as np
 import myRFLibs
+import numpy as np
+import RF_Model as model
+import streamlit as st
+from PIL import Image, ImageOps
 
-# Init node
+
+# Init node (Bắt buộc phải giữ lại ở đây để pickle load không bị lỗi __main__)
 class Node:
-    def __init__(self, feature_idx=None, threshold=None, left=None, right=None, value=None):
-        self.feature_idx = feature_idx    
-        self.threshold = threshold  
-        self.left = left                  
-        self.right = right                
-        self.value = value                
+
+    def __init__(
+        self,
+        feature_idx=None,
+        threshold=None,
+        left=None,
+        right=None,
+        value=None,
+    ):
+        self.feature_idx = feature_idx
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
 
     def is_leaf(self):
         return self.value is not None
-    
+
+
 # Load 99% accuracy model
 forestNumber = model.load_model("forestNumber.pkl")
 
-# Load MINIST image 
+st.title("MNIST Handwritten Digit Classifier")
+
+# Load MNIST image
+MINISTImg = st.file_uploader(
+    "Drag an image here", type=["png", "jpg", "jpeg"]
+)
+
+# Tạo một biến toàn cục để chứa ảnh đã xử lý làm phẳng
 img_flatten = None
-MINISTImg = st.file_uploader("Drag an image here", type=["png", "jpg", "jpeg"])
-img_np = np.array(MINISTImg, dtype=np.float32)
+
 # CHỈ XỬ LÝ KHI NGƯỜI DÙNG ĐÃ TẢI ẢNH LÊN
 if MINISTImg is not None:
     # 1. Mở ảnh gốc bằng PIL và hiển thị lên giao diện cho người dùng xem
@@ -53,9 +69,7 @@ else:
     if "result" in st.session_state:
         del st.session_state.result
     st.info("Please upload an image of a handwritten digit to proceed.")
-    
-#---UI---#
-st.title("MNIST Handwritten Digit Classifier")
+
 # NÚT BẤM DỰ ĐOÁN
 if st.button("Predict"):
     if img_flatten is not None:
@@ -73,7 +87,5 @@ if st.button("Predict"):
 # HIỂN THỊ KẾT QUẢ
 if "result" in st.session_state:
     st.header(f"A Predicted number is: :green[{st.session_state.result}]")
-    
-    
-st.divider()
 
+st.divider()
